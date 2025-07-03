558fa0d0f41f7d77b55ca6d4102e5ea84e33bf7e
Academy Training <academytraining@VDARTINC.COM>
2025-07-03 15:16:42 +0530

    Initial spring

---
+++ vdartcourses/src/main/java/com/vdart/vdartcourses/VdartcoursesApplication.java
@@ -0,0 +1,13 @@
+package com.vdart.vdartcourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdartcoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdartcoursesApplication.class, args);
+	}
+
+}
