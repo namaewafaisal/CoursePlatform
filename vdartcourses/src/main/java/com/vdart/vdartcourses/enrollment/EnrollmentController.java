45773967c4de2f3cae92f0be9aac38cffe3009ca
namaewafaisal <mohamed.faisal.4626@gmail.com>
2025-07-22 07:59:22 -0400

    Fix: Enrollment again delete

--- vdartcourses/src/main/java/com/vdart/vdartcourses/enrollment/EnrollmentController.java
+++ vdartcourses/src/main/java/com/vdart/vdartcourses/enrollment/EnrollmentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -42,6 +45,19 @@ public class EnrollmentController {
     @PreAuthorize("hasRole('ADMIN')")
     public List<Enrollment> getAllEnrollments() {
         return enrollmentService.getAllEnrollments();
+    }
+
+    @DeleteMapping("/enrollmentid/{id}/delete")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deleteEnrollment(@PathVariable ObjectId id) {
+        enrollmentService.deleteEnrollment(id);
+    }
+
+    @PutMapping("/enrollmentid/{id}/update")
+    @PreAuthorize("isAuthenticated()")
+    public Enrollment updateEnrollment(@PathVariable ObjectId id, @RequestBody Enrollment enrollment) {
+        
+        return enrollmentService.updateEnrollment(id,enrollment);
     }
     
       
--- vdartcourses/src/main/java/com/vdart/vdartcourses/enrollment/EnrollmentService.java
+++ vdartcourses/src/main/java/com/vdart/vdartcourses/enrollment/EnrollmentService.java
@@ -2,12 +2,14 @@ package com.vdart.vdartcourses.enrollment;
 
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.vdart.vdartcourses.service.CurrentUserService;
+import com.vdart.vdartcourses.service.ResourceNotFoundException;
 
 
 @Service
@@ -35,5 +37,29 @@ public class EnrollmentService {
     public List<Enrollment> getAllEnrollments() {
         return enrollmentRepo.findAll();
     }
-}
 
+    public void deleteEnrollment(ObjectId id) {
+        enrollmentRepo.deleteById(id);
+    }
+
+    public Enrollment updateEnrollment(ObjectId id, Enrollment enrollment) {
+        String username = currentUserService.getUsername();
+        Enrollment enrollment2 = enrollmentRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Enrollment not found with id: " + id));
+        if(!(enrollment2.getUsername().equals(username))) {
+            throw new ResourceNotFoundException("You are not authorized to update this enrollment.");       
+        }
+        if(enrollment.getCourseId() != null) {
+            enrollment2.setCourseId(enrollment.getCourseId());
+        }
+        if(enrollment.getUsername()!= null) {
+            enrollment2.setUsername(enrollment.getUsername());
+        }
+        if(enrollment.getEnrollmentDate()!= null) {
+            enrollment2.setEnrollmentDate(enrollment.getEnrollmentDate());
+        }
+        if(enrollment.getProgress()!= null) {
+            enrollment2.setProgress(enrollment.getProgress());
+        }
+        return enrollmentRepo.save(enrollment2);
+    }
+}
