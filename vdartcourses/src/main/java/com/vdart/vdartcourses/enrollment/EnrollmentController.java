bd8a38ee334a055af52e0e85b6c35aaaeacbd726
namaewafaisal <mohamed.faisal.4626@gmail.com>
2025-07-22 05:50:40 -0400

    Fix:Subtopics

--- vdartcourses/src/main/java/com/vdart/vdartcourses/enrollment/EnrollmentController.java
+++ vdartcourses/src/main/java/com/vdart/vdartcourses/enrollment/EnrollmentController.java
@@ -36,7 +36,5 @@ public class EnrollmentController {
         enrollmentService.enrollUserInCourse(enrollment);
         
     }
-    
-    
-    
+      
 }
--- vdartcourses/src/main/java/com/vdart/vdartcourses/subtopic/SubtopicController.java
+++ vdartcourses/src/main/java/com/vdart/vdartcourses/subtopic/SubtopicController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,22 +22,26 @@ public class SubtopicController {
 
     // Add a new subtopic to a course
     @PostMapping("/courseid/{courseId}/add")
+    @PreAuthorize("hasRole('ADMIN')")   
     public Subtopic addSubtopic(@PathVariable ObjectId courseId, @RequestBody Subtopic subtopic) {
         return subtopicService.saveSubtopic(subtopic, courseId);
     }
 
     // Get all subtopics for a specific course
     @GetMapping("/courseid/{courseId}/all")
+    @PreAuthorize("isAuthenticated()")   
     public List<Subtopic> getSubtopicsByCourseId(@PathVariable String courseId) {
         return subtopicService.getSubtopicsByCourseId(courseId);
     }
     // Delete a subtopic by ID
     @DeleteMapping("/subtopicid/{id}/delete")
+    @PreAuthorize("hasRole('ADMIN')")   
     public ResponseEntity<String> deleteSubtopicById(@PathVariable ObjectId id) {
         subtopicService.deleteById(id);
         return ResponseEntity.ok("Subtopic deleted successfully");
     }
     @GetMapping("/all")
+    @PreAuthorize("hasRole('ADMIN')")   
     public List<Subtopic> getAllSubtopics() {
         return subtopicService.getAllSubtopics();
     }
